93e4835862d5e7b523c8c7b8b8f6e1b2a4f9b448
sadityaprdxn <adityas@prdxn.com>
2020-08-19 11:55:15 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState , useEffect } from 'react';
+import '../scss/App.scss';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+const App = () => {
+
+  return (
+    <h1>hello world</h1>
+  );
+}
+
+export default App;
